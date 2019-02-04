ec30aecc90f19aa5b9aff2e4e7a33b838f6366f1
zhurof <41473707+zhurof@users.noreply.github.com>
2019-02-04 17:07:17 +0200

    day

--- index.php
+++ index.php
@@ -25,5 +25,79 @@
         </form>
       </div>      
     </div>
+    <div class="simile">
+      <div class="simile__item">
+        <div class="btn btn_white simile__btn">
+          <a href="#">3D проект</a>
+        </div>
+        <img src="img/before.jpeg" alt="">
+      </div>
+      <div class="simile__item">
+        <div class="btn btn_white simile__btn">
+          <a href="#">Реализация</a>
+        </div>
+        <img src="img/after.jpeg" alt="">
+      </div>
+    </div>
+    <section class="section directions">
+      <div class="wrapper">
+        <h2 class="section__title">Создаем и реализуем дизайн-проекты <br>интерьеров любой сложности</h2>
+        <div class="directions__content">
+          <div class="box directions__box">
+            <div class="dir-card">
+              <div class="dir-card__pic">
+                <img src="img/directions/1.jpg" alt="">
+              </div>
+              <div class="dir-card__icon">
+                <img src="img/icons/appartments.svg" alt="">
+              </div>
+              <div class="dir-card__body">
+                <h3 class="dir-card__title">Дизайн квартир</h3>
+                <div class="dir-card__text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+                </div>
+              </div>
+              <div class="dir-card__footer">
+                <div class="btn btn_thin btn_small"><a href="#">Смотреть работы</a></div>
+              </div>
+            </div>
+            <div class="dir-card">
+              <div class="dir-card__pic">
+                <img src="img/directions/2.jpg" alt="">
+              </div>
+              <div class="dir-card__icon">
+                <img src="img/icons/house.svg" alt="">
+              </div>
+              <div class="dir-card__body">
+                <h3 class="dir-card__title">Дизайн домов</h3>
+                <div class="dir-card__text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+                </div>
+              </div>
+              <div class="dir-card__footer">
+                <div class="btn btn_thin btn_small"><a href="#">Смотреть работы</a></div>
+              </div>
+            </div>
+            <div class="dir-card">
+              <div class="dir-card__pic">
+                <img src="img/directions/3.jpg" alt="">
+              </div>
+              <div class="dir-card__icon">
+                <img src="img/icons/commerce.svg" alt="">
+              </div>
+              <div class="dir-card__body">
+                <h3 class="dir-card__title">Дизайн коммерческих помещений</h3>
+                <div class="dir-card__text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes 
+                </div>
+              </div>
+              <div class="dir-card__footer">
+                <div class="btn btn_thin btn_small"><a href="#">Смотреть работы</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 	</main>
 <?php	include "footer.php";?>	
