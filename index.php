dc67ec242cd12b4a82b483cbd601c6ceb04d2de5
zhurof <41473707+zhurof@users.noreply.github.com>
2019-02-04 15:16:14 +0200

    top block done

--- index.php
+++ index.php
@@ -4,13 +4,26 @@
       <div class="wrapper">
         <h1 class="top-block__title">ДИЗАЙН <br>ИНТЕРЬЕРА ОТ ПРОФИ</h1>
         <div class="top-block__text">по Москве и области</div>
-      </div>
-      <form action="" class="step-form top-block__form">
-        <h3 class="step-form__title">Рассчитайте стоимость дизайна за 3 шага</h3>
-        <div class="step-form__body">
-          
-        </div>
-      </form>
+        <form action="" class="step-form top-block__form">
+          <h3 class="step-form__title">Рассчитайте стоимость дизайна за 3 шага</h3>
+          <div class="step-form__body">
+            <div class="step-form__item">
+              <input type="text" placeholder="Площадь объекта">
+            </div>
+            <div class="step-form__item">
+              <select>
+                <option value="" disabled selected>Тип объекта:</option>
+                <option value="">Квартира</option>
+                <option value="">Коттедж</option>
+              </select>
+            </div>
+            <div class="step-form__item">
+              <input type="tel" placeholder="Ваш телефон">
+              <div class="btn"><input type="submit" value="Готово"></div>
+            </div>
+          </div>
+        </form>
+      </div>      
     </div>
 	</main>
 <?php	include "footer.php";?>	
--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-﻿
+﻿//Пошаговая форма
+$('.step-form__body').on('init reInit',function(){
+  $(this).find('.step-form__item:not(:last-child)').append('<div class="btn step-form__next"><a href="#">Далее</a></div>');
+  $(this).append('<div class="step-form__progress"><span></span></div>');
+})
+$('.step-form__body').on('click','.step-form__next', function(e){
+  e.preventDefault();
+  $(this).parents('.step-form__body').slick('slickNext');
+})
+$('.step-form__body').on('init reInit afterChange',function(event,slick,currentSlide){
+  var i =  (currentSlide ? currentSlide : 0) + 1,
+      part = i/slick.slideCount*100 + '%';
+  $(this).parents('.step-form').find('.step-form__progress span').width(part);
+})
+$('.step-form__body').slick({
+  accessibility:false,
+  arrows:false,
+  dots:true,
+  speed:300,
+  dotsClass:'step-form__nav',
+  fade:true,
+  infinite:false,
+  customPaging:function(slider,i){return i+1}
+})
