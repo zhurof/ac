54f21b0d0b20ee4dd31372e9fd9aa75dc6c691b8
zhurof <41473707+zhurof@users.noreply.github.com>
2019-02-06 11:14:44 +0200

    index-page desktop
    desktop version of homepage done

--- footer.php
+++ footer.php
@@ -1,5 +1,28 @@
-	<footer>
-		
+	<footer class="footer">
+		<div class="wrapper footer__wrapper">
+      <div class="logo footer__logo"><a href="/"><img src="img/logo.png" alt=""></a></div>
+      <div class="footer__main">
+        <div class="footer__menu">
+          <ul>
+            <li><a href="design.php">Дизайн квартир</a></li>
+            <li><a href="#">Стили дизайна</a></li>
+            <li><a href="#">Услуги</a></li>
+            <li><a href="#">Блог</a></li>
+            <li><a href="#">Дизайн домов</a></li>
+            <li><a href="#">Отзывы</a></li>
+            <li><a href="#">Контакты</a></li>
+            <li><a href="#">Портфолио</a></li>
+            <li><a href="#">Бонусы</a></li>
+          </ul>
+        </div>
+        <div class="footer__address">м. Курская, Нижний Сусальный переулок, 5 строение 17 Территория творческого завода "ARMA"</div>
+      </div>
+      <div class="footer__contacts">
+        <div class="phone footer__phone">
+          <a href="tel:74996740671">+7 (499) 674-06-71</a>
+        </div>
+      </div>
+    </div>
 	</footer>
 	<script src="//code.jquery.com/jquery-latest.min.js"></script>
 	<script src="libs/slick/slick.min.js"></script>
--- index.php
+++ index.php
@@ -423,5 +423,35 @@
         </div>
       </div>
     </section>
+    <section class="section consult">
+      <div class="wrapper">
+        <div class="consult__content">
+          <div class="consult__main">
+            <h2 class="consult__title">Хорошо что вы тут!</h2>
+            <div class="consult__text">
+              <p>Поздравляю! Вы докрутили сайт до самого конца. Не уходите с пустыми руками. Воспользуйтесь спецпредложением от меня:</p>
+              <p><big>Бесплатный выезд специалиста <br>на ваш объект <em>+ консультация</em></big></p>
+            </div>
+            <form action="" class="consult__form">
+              <div class="consult__field">
+                <input type="text" placeholder="Ваше имя:" />
+              </div>
+              <div class="consult__field">
+                <input type="tel" placeholder="Ваше телефон:" />
+              </div>
+              <div class="btn btn_block consult__btn"><input type="submit" value="Получить консультацию"></div>
+              <div class="consult__label">
+                <input type="checkbox" id="cns" checked>
+                <label for="cns">Я принимаю политику обработки <br/><a href="#">персональных данных</a>.</label>
+              </div>
+            </form>
+          </div>
+          <div class="consult__author">
+            <h4 class="consult__author-name">Артём Болдырев</h4>
+            <span>главный дизайнер и основатель студии "АС Бюро"</span>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 <?php	include "footer.php";?>	
